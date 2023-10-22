2058f8c1e3ed54445c5efd314c741ed41944da59
Albert <albert.esparrell@gmail.com>
2023-10-22 18:58:19 +0200

    Update files
    Just update temp-cpu.sh

--- tint2/Executors/temp-cpu.sh
+++ tint2/Executors/temp-cpu.sh
@@ -6,4 +6,14 @@ tempsystin=`sensors | awk '/SYSTIN/ {print $2+0}'`
 
 cargasistema=`cat /proc/loadavg | cut -d ' ' -f 1`
 
-echo "  $tempcputin°C $tempsystin°C $temptctl°C |  $cargasistema | "
+if [ $tempcputin -gt 45 ]
+then
+dunstify "Cuidado!" "Procesador esta a $tempcputin°C o más." -u critical
+fi
+
+if [ $tempsystin -gt 45 ]
+then
+dunstify "Cuidado!" "Placa Base esta a $tempsystin°C o más." -u critical
+fi
+
+echo "  $temptctl°C |  $cargasistema | "
