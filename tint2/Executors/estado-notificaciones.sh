af8297c9a0e47a5d805dbff379d7d52ae868c60d
Albert <albert.esparrell@gmail.com>
2023-12-11 18:55:50 +0100

    Delete tint2 directory

--- tint2/Executors/estado-notificaciones.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Comprueba si dunst tiene las notificaciones en pausa o no, muestra una campana por pantalla en la barra de tareas.
-if [ `dunstctl is-paused` = 'false' ]
-then
-echo " ON | "
-else
-echo " OFF | "
-fi
